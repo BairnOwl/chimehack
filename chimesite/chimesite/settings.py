d01a6e21aceda9634ecd8fc92dfc4bbcb8052ca0
ganewman42 <ganewman42@gmail.com>
2016-08-28 11:14:43 -0700

    Undid revert and resulting problems

--- chimesite/chimesite/settings.py
+++ chimesite/chimesite/settings.py
@@ -80,8 +80,8 @@ WSGI_APPLICATION = 'chimesite.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': os.path.join(BASE_DIR, 'db.mysql'),
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
