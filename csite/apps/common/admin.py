e9ea1375c8ccac10ad2892c63ec35d6d8d2dad5a
ganewman42 <ganewman42@gmail.com>
2016-08-28 10:45:49 -0700

    Revert "google app engine deployment"
    This reverts commit b58e18d2385de0fc58fe13290c772a98ae02f225.

--- csite/apps/common/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from .models import Country, Location
-
-# Register your models here.
-
-admin.site.register(Country)
-admin.site.register(Location)
--- csite/apps/common/urls.py
+++
@@ -1,7 +0,0 @@
-from django.conf.urls import url
-
-import views
-
-urlpatterns = [
-    url(r'^emergency/', views.country_emergency_number, name='emergencynumber'),
-]
