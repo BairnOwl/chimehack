3dc967c1578e41134ec2aab300c5ad5cc7aa7625
Linda Chang <linda_chang@brown.edu>
2016-08-27 20:28:05 -0700

    basic twilio implementation

--- server.js
+++ server.js
@@ -2,15 +2,46 @@
  * Created by bairnowl on 8/27/16.
  */
 
+var accountSid = 'ACd11cce760ddc05c7eef7fe3448030342'; // Your Account SID from www.twilio.com/console
+var authToken = '6b7dfc77a039c8d60461a562441fdeb0';   // Your Auth Token from www.twilio.com/console
+
 var twilio = require('twilio');
+var client = new twilio.RestClient(accountSid, authToken);
 
 var express = require('express');
 var app = express();
 
 
-app.listen(process.env.PORT, function () {
-    console.log('HerStory app listening on port ' + process.env.PORT);
+// Twilio interface
+
+app.post('/incoming', function(req, res) {
+    var phoneNumber = req.body.From;
+    var message = parseInt(req.body.Body);
+
+    console.log(choice);
+
+    if (!isNaN(message)) {
+        if (message == 'her') {
+            sendIntroMessage(phoneNumber);
+        }
+
+    } else {
+        //sendErrorMessage(phoneNumber);
+    }
 });
+
+function sendIntroMessage(phoneNumber) {
+    client.messages.create({
+        body: 'I\'m here to help. I won\'t share any personal information that you share with me. ' +
+        'Other women have gone through this.\nPress\n(1) to read their stories\n(2) to find local resources',
+        to: phoneNumber,
+        from: '+14017533904'
+    }, function (err, message) {
+        if (err) {
+            console.error(err.message);
+        }
+    });
+}
 
 app.get('/home', function(request, response){
    response.send("hellooooooooo world"); 
@@ -57,4 +88,8 @@ app.get('/matching', function(req, res) {
 app.get('/orginfo', function(req, res) {
     //make call to django app
   res.send('Hello World!');
-}); 
+});
+
+app.listen(process.env.PORT, function () {
+    console.log('HerStory app listening on port ' + process.env.PORT);
+});
