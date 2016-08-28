f25401f474feed539bcef09a597f148344472ebb
Linda Chang <linda_chang@brown.edu>
2016-08-27 17:33:39 -0700

    skeleton server

--- server.js
+++ server.js
@@ -2,3 +2,12 @@
  * Created by bairnowl on 8/27/16.
  */
 
+var twilio = require('twilio');
+
+var express = require('express');
+var app = express();
+
+
+app.listen(process.env.PORT, function () {
+    console.log('HerStory app listening on port ' + process.env.PORT);
+});
